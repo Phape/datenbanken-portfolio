7c9334b8fbd60628c8cfb861cc16ca823b379be0
FaNaSKY <55694295+FaNaSKY@users.noreply.github.com>
2020-10-08 22:26:58 +0200

    TestSession sql Datei hinzugefügt
    Eigene Kopie (für mich) zu Testzwecken hinzugefügt

--- dbPortfolio.session.sql
+++ dbPortfolio.session.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE dbPortfolio;
+CREATE DATABASE dbportfolio;
+USE dbportfolio;
 CREATE TABLE rentner (
     Versicherungs_Nr INTEGER,
     Nachname VARCHAR(70), 
@@ -16,13 +17,21 @@ CREATE TABLE vertrag (
     Versicherungs_Nr INTEGER,
     PRIMARY KEY (Vertrags_ID)
 );
+CREATE TABLE adresse (
+    Adress_ID INTEGER,
+    Straße VARCHAR(70),
+    Hausnummer VARCHAR(10),
+    Postleitzahl INTEGER,
+    PRIMARY KEY (Adress_ID)
+);
 CREATE TABLE arbeitgeber (
     Arbeitgeber_ID INTEGER,
     Adress_ID INTEGER,
     Firmenname VARCHAR(120),
     Mitarbeiter_Nr INTEGER,
     Abrechnungsverband VARCHAR(10),
-    PRIMARY KEY (Arbeitgeber_ID)
+    PRIMARY KEY (Arbeitgeber_ID),
+    FOREIGN KEY (Adress_ID) REFERENCES adresse(Adress_ID)
 );
 CREATE TABLE key_account_manager (
     Mitarbeiter_Nr INTEGER,
@@ -31,13 +40,7 @@ CREATE TABLE key_account_manager (
     Eintrittsdatum DATE,
     PRIMARY KEY (Mitarbeiter_Nr)
 );
-CREATE TABLE adresse (
-    Adress_ID INTEGER,
-    Straße VARCHAR(70),
-    Hausnummer VARCHAR(10),
-    Postleitzahl INTEGER,
-    PRIMARY KEY (Adress_ID)
-);
+
 CREATE TABLE ort (
     Postleitzahl INTEGER,
     Ortsname VARCHAR(70),
@@ -54,37 +57,33 @@ CREATE TABLE versicherter (
     PRIMARY KEY (Versicherungs_Nr)
 );
 
-SAVEPOINT vor_insert;
-
-INSERT INTO rentner (Versicherungs_Nr, Nachname, Vorname, Rentenart, Rentenhoehe, Arbeitgeber_ID)
-VALUES ('Jutta', 'Müller', '0819', 3500), --beispiele, bitte mit unseren Daten füllen
-(NULL, 'Groß', '0820', NULL);
-
-
-INSERT INTO vertrag (Vertrags_ID, Vertragsstatus, Typ, Abschlussdatum, Versicherungs_Nr)
-Values ('101', ),
-(); --bitte Beispieldaten einfügen
-
-
-INSERT INTO arbeitgeber (Arbeitgeber_ID, Adress_ID, Firmenname, Mitarbeiter_Nr, Abrechnungsverband)
-VALUES (),
-(), --bitte Beispieldaten einfügen
-();
-
-
-INSERT INTO key_account_manager (Mitarbeiter_Nr, Vorname, Nachname, Eintrittsdatum)
-VALUES (),
-(), --bitte Beispieldaten einfügen
-();
-
-
-INSERT INTO adresse (Adress_ID, Straße, Hausnummer, Postleitzahl)
-VALUES (),
-(), --bitte Beispieldaten einfügen
-();
-
-
-INSERT INTO ort (Postleitzahl, Ortsname)
-VALUES (),
-(), --bitte Beispieldaten einfügen
-();
+INSERT INTO vertrag (
+    Vertrags_ID,
+    Vertragsstatus,
+    Typ,
+    Abschlussdatum,
+    Versicherungs_Nr
+  )
+VALUES (
+    5646,
+    'Betriebsrente',
+    'klassik',
+    '1966-12-07',
+    123456789
+  );
+  INSERT INTO rentner (
+      Versicherungs_Nr,
+      Nachname,
+      Vorname,
+      Rentenart,
+      Rentenhoehe,
+      Arbeitgeber_ID
+    )
+  VALUES (
+      123456789,
+      'Hallo',
+      'Welt',
+      'klassik',
+      '251',
+      83325
+    );
