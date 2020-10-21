a54189baf069cfc0755dd6a0ef575d26b69f89ca
Philipp Mayer <philipp.mayer@phape.de>
2020-10-21 15:40:36 +0200

    added maven project

---
+++ src/main/java/de/gruppe4/datenbankenportfolio/App.java
@@ -0,0 +1,13 @@
+package de.gruppe4.datenbankenportfolio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/de/gruppe4/datenbankenportfolio/AppTest.java
@@ -0,0 +1,20 @@
+package de.gruppe4.datenbankenportfolio;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
